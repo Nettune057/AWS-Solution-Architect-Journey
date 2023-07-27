4046bc54a7c385826f3f5ec6f921e3d50c94b867
tungthd2303@gmail.com <tungnt@kaopiz.com>
2023-07-27 11:23:06 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# SAM Serverless architecture
+
+This is the SAM example for Rest API - Cognito - Lambda - DynamoDB
+
+![alt text](./simple.png)
+
+## Progresses
+
+### 24/7/2023 
+Completed Rest API - Cognito -Lambda - DynamoDB, ready for authorization with Cognito
+
+### 25/7/2023
+Researched Cognito 
+
+
+## Test Usage
+curl -X "PUT" -H "Content-Type: application/json" -d "{\"id\": \"123\", \"price\": 12345, \"name\": \"myitem\"}" https://{apiID}.execute-api.us-east-2.amazonaws.com/Prod/items
+
+export COGNITO_USER_EMAIL='a@gmail.com'
+
+sam deploy --parameter-overrides CognitoUserEmail=$COGNITO_USER_EMAIL
+
+aws cognito-idp admin-set-user-password --user-pool-id --username  --password --permanent
+
+aws cognito-idp initiate-auth --auth-flow USER_PASSWORD_AUTH --auth-parameters "USERNAME=$COGNITO_USER_EMAIL,PASSWORD=" --client-id 
---
+++ src/handlers/example/index.mjs
@@ -0,0 +1,72 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "http-crud-tutorial-items";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  console.log(event);
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    switch (event.httpMethod) {
+      case "DELETE":
+        await dynamo.send(
+          new DeleteCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET":
+        body = await dynamo.send(
+          new ScanCommand({ TableName: tableName })
+        );
+        body = body.Items;
+        break;
+      case "PUT":
+        let requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              id: requestJSON.id,
+              price: requestJSON.price,
+              name: requestJSON.name,
+            },
+          })
+        );
+        body = `Put item ${requestJSON.id}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
